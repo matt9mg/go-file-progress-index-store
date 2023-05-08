99c9f946ae37b07f6a02ee59ae11f585bb1ba25a
mt <matt9mg@hotmail.com>
2023-05-08 22:42:27 +0100

    wip

--- file_progress_store.go
+++ file_progress_store.go
@@ -3,15 +3,16 @@ package file_progress_store
 import (
 	"encoding/json"
 	"github.com/pkg/errors"
+	"io"
 	"os"
 )
 
 type indexTypes interface {
 	string | int | int8 | int16 | int32 | int64 | float32 | float64
 }
-
 type FileProgressStore[T indexTypes] struct {
-	Results []T
+	results []T
+	file    *os.File
 }
 
 func NewFileProgressStore[T indexTypes](fileStore string) (*FileProgressStore[T], error) {
@@ -21,17 +22,45 @@ func NewFileProgressStore[T indexTypes](fileStore string) (*FileProgressStore[T]
 		}
 	}
 
-	f, err := os.Open(fileStore)
+	f, err := os.OpenFile(fileStore, os.O_RDWR, os.ModePerm)
 
 	if err != nil {
 		return nil, errors.New("unable to open file " + fileStore + " with error: " + err.Error())
 	}
 
-	fps := &FileProgressStore[T]{}
-
-	if err = json.NewDecoder(f).Decode(&fps.Results); err != nil {
-		return fps, errors.New("unable to bind results to FileProgressStore with error: " + err.Error())
+	fps := &FileProgressStore[T]{
+		file: f,
 	}
 
-	return fps, nil
+	if err = json.NewDecoder(f).Decode(&fps.results); err == io.EOF {
+		return fps, nil
+	}
+
+	if err == nil {
+		return fps, nil
+	}
+
+	return nil, errors.New("unable to bind results to FileProgressStore with error: " + err.Error())
+}
+
+func (f *FileProgressStore[T]) Save(items ...T) error {
+	f.results = append(f.results, items...)
+	data, err := json.Marshal(f.results)
+
+	if err != nil {
+		return err
+	}
+
+	if err = f.file.Truncate(0); err != nil {
+		return err
+	}
+	_, err = f.file.Seek(0, 0)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = f.file.Write(data)
+
+	return err
 }
